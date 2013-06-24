526de7140ddbbe39aad0b48938ca849a99d3f8b2
Alice <alice@example.com>
2013-06-24 22:12:27 +0900

    showメソッドを簡潔に修正
    input メソッドを追加

--- what_is_git.rb
+++ what_is_git.rb
@@ -4,14 +4,18 @@ Class WhatIsGit
   end
 
   def show
-    puts 'Do you understand the basis [yes/no]'
-    input = gets.shomp.downcase
-    case input
+    case input ('Do you understand the basis [yes/no]')
     when 'yes'
       puts 'Git is easy
     else
       puts 'Git is difficult...'
     end
   end
+
+  private
+    def input(message)
+      puts message
+      gets.chomp.downcase
+    end
 
 end
