d8bfa9b0faf99b160e7d005528934fd1b252e6b8
Alice <alice@example.com>
2013-06-24 21:35:42 +0900

    showメソッドのメッセージを修正
    先頭を大文字に変更

--- what_is_git.rb
+++ what_is_git.rb
@@ -1,9 +1,9 @@
 Class WhatIsGit
   def about(lang = 'en')
-    puts 'http://#{lang".wikipedia.org/wiki/Git'
+    puts 'http://#{lang}.wikipedia.org/wiki/Git'
   end
 
   def show
-    puts 'if you understand the basic, Git is easy.'
+    puts 'If you understand the basic, Git is easy.'
   end
 end
